2b100525475c1e63508ddc28c18eaa293ea1280b
Rabinkhulimuli <131673248+Rabinkhulimuli@users.noreply.github.com>
2026-09-07 20:58:10 +0545

    hotfix: hydration error fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' className={cn('font-sans', geist.variable)}>
-      <body className='antialiased'>
+      <body className='antialiased' suppressHydrationWarning>
         {children}
         {process.env.NODE_ENV === 'production' && <Analytics />}
       </body>
